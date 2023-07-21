b7ebb35f274d9a174eff7ee3f24f054add1d7b4c
elahe919 <elahehbeigi512@gmail.com>
2023-07-22 00:36:23 +0330

    fix bugs

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
-let resultDisplay = document.querySelector(".result-display");
-
-function showItem(event) {
-  const a = event.target.innerText;
-  if (resultDisplay.innerHTML == 0) {
-    return resultDisplay.innerHTML = a;
-  } 
-    return resultDisplay.innerHTML += a;
-}
-
-function calculate(){
-    let cal = resultDisplay.innerText;
-    resultDisplay.innerText = eval(cal);
-}
-
-function clearAll() {
-  resultDisplay.innerText = 0;
-}
-
-function clear() {
-  let displayText = resultDisplay.innerText;
-  if (displayText.length === 1) {
-    resultDisplay.innerText = 0;
-  } else {
-    resultDisplay.innerText = displayText.substring(0, displayText.length - 1);
-  }
-}
-
+let displayBox = document.querySelector(".display");
 
 document.querySelector(".calculate").addEventListener("click", calculate);
-document.querySelector(".clear-all").addEventListener("click", clearAll);
+document.querySelector(".all-clear").addEventListener("click", allClear);
 document.querySelector(".clear-last").addEventListener("click", clear);
 
-let showKey = document.querySelectorAll(".key");
+
+let showKey = document.querySelectorAll(".show-display");
 showKey.forEach(element => {
     element.addEventListener("click", showItem);
   });
+
+
+function showItem(event) {
+  const a = event.target.innerText;
+  if (displayBox.innerHTML == 0) {
+    return displayBox.innerHTML = a;
+  } 
+    return displayBox.innerHTML += a;
+}
+
+function calculate(){
+    let cal = displayBox.innerText;
+    displayBox.innerText = eval(cal);
+}
+
+function allClear() {
+  displayBox.innerText = 0;
+}
+
+function clear() {
+  let displayText = displayBox.innerText;
+  if (displayText.length === 1) {
+    displayBox.innerText = 0;
+  } else {
+    displayBox.innerText = displayText.substring(0, displayText.length - 1);
+  }
+}
+
 
 
